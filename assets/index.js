88088bef5e868ff7be17382aeaf3d1bec0fb1ca4
Clouser <clouser24ryan@gmail.com>
2024-11-09 15:38:20 -0500

    collision added

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,12 @@ document.addEventListener('keydown', (event) => {
         jump();
     }
 });
+
+var checkCollision = setInterval(function(){
+    var charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"));
+    var objectLeft = parseInt(window.getComputedStyle(object).getPropertyValue("left"));
+    if(objectLeft < 30 && objectLeft > 0 && charTop > 216){
+        object.style.animation = "none";
+        console.log("You Lose");
+    }
+}, 10)
