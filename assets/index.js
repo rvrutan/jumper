f80f28adf8b047090984375fdc9b84e3bb8d335c
rvrutan <rutanroni@gmail.com>
2024-11-09 00:11:14 -0500

    added framework

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,21 @@ document.addEventListener('keydown', (event) => {
         jump();
     }
 });
+
+
+
+
+//mode switch
+
+// const toggleMode = () => {
+//     const htmlNode = document.querySelector('html').getAttributeNode("data-bs-theme");
+//     const modeSwitch = document.querySelectorAll('#modeSwitchContainer > input');
+//     for (let i = 0; i < modeSwitch.length; i++) {
+//         modeSwitch[i].addEventListener('click' , () => {
+//             modeSwitch[i].checked ? htmlNode.value = 'light' : htmlNode.value = 'dark'
+
+//         });
+//     }
+// }
+
+// toggleMode();
